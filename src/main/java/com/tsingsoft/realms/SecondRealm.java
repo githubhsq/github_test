781cda11709bc757eb4041ab7c7c02862a5403e6
hsq <nmg_hsq@163.com>
2018-05-09 00:44:36 +0800

    多realm认证

--- src/main/java/com/tsingsoft/realms/SecondRealm.java
+++ src/main/java/com/tsingsoft/realms/SecondRealm.java
@@ -1,24 +1,15 @@
 package com.tsingsoft.realms;
 
-import com.sun.tools.classfile.Opcode;
 import org.apache.shiro.authc.*;
-import org.apache.shiro.authz.AuthorizationInfo;
-import org.apache.shiro.authz.SimpleAuthorizationInfo;
 import org.apache.shiro.crypto.hash.SimpleHash;
 import org.apache.shiro.realm.AuthenticatingRealm;
-import org.apache.shiro.realm.AuthorizingRealm;
-import org.apache.shiro.realm.Realm;
-import org.apache.shiro.subject.PrincipalCollection;
 import org.apache.shiro.util.ByteSource;
 
-import java.util.HashSet;
-import java.util.Set;
-
-public class ShiroRealm extends AuthorizingRealm{
+public class SecondRealm extends AuthenticatingRealm{
 
     @Override
     protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
-        System.out.println("firstRealm: " + authenticationToken.hashCode());
+        System.out.println("secodeRealm: " + authenticationToken.hashCode());
         UsernamePasswordToken token = (UsernamePasswordToken) authenticationToken;
         String username = token.getUsername();
         if (username.equals("hsq")){
@@ -30,36 +21,24 @@ public class ShiroRealm extends AuthorizingRealm{
         Object principal = username;
         Object credentials = null;
         if (username.equals("admin")){
-             credentials = "038bdaf98f2037b31f1e75b5b4c9b26e";
+             credentials = "ce2f6417c7e1d32c1d81a797ee0b499f87c5de06";
         }else  if (username.equals("user")){
-             credentials = "098d2c478e9c11555ce2823231e02ec1";
+             credentials = "073d4c3ae812935f23cb3f2a71943f49e082a718";
         }
 
         String realName = getName();
         ByteSource salt = ByteSource.Util.bytes(username);
 //        SimpleAuthenticationInfo info = new SimpleAuthenticationInfo(principal,credentials,realName);
-        SimpleAuthenticationInfo info = new SimpleAuthenticationInfo(principal,credentials,salt,realName);
+        SimpleAuthenticationInfo info = new SimpleAuthenticationInfo("secondrealm",credentials,salt,realName);
         return info;
     }
 
     public static void main(String args[]){
-        String algorithmName = "MD5";
+        String algorithmName = "SHA1";
         Object credentials = "123456";
-        Object salt = ByteSource.Util.bytes("admin");
+        Object salt = ByteSource.Util.bytes("user");
         int hashIterations = 1024;
         Object result = new SimpleHash(algorithmName,credentials,salt,hashIterations);
         System.out.println(result);
-    }
-
-    @Override
-    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
-        Object principal = principals.getPrimaryPrincipal();
-        Set<String> roles = new HashSet<>();
-        roles.add("user");
-        if (principal.equals("admin")){
-            roles.add("admin");
-        }
-        SimpleAuthorizationInfo info = new SimpleAuthorizationInfo(roles);
-        return info;
     }
 }
--- src/main/webapp/list.jsp
+++ src/main/webapp/list.jsp
@@ -12,17 +12,17 @@
 	
 	<h4>List Page</h4>
 	
-	Welcome: <shiro:principal></shiro:principal>
-	
-	<shiro:hasRole name="admin">
+	<%--Welcome: <shiro:principal></shiro:principal>--%>
+
+	<%--<shiro:hasRole name="admin">--%>
 	<br><br>
 	<a href="admin.jsp">Admin Page</a>
-	</shiro:hasRole>
-	
-	<shiro:hasRole name="user">
+	<%--</shiro:hasRole>--%>
+
+	<%--<shiro:hasRole name="user">--%>
 	<br><br>
 	<a href="user.jsp">User Page</a>
-	</shiro:hasRole>
+	<%--</shiro:hasRole>--%>
 	
 	<br><br>
 	<a href="shiro/testShiroAnnotation">Test ShiroAnnotation</a>
