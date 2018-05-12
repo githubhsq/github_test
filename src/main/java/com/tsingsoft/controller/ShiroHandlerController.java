2e8cf39a57ee95b1543c0fec45c94b3f7faa88a0
nmghsq <shqhe@tsingsoft.com.cn>
2018-05-12 18:45:10 +0800

    shiro 缓存配置

--- src/main/java/com/tsingsoft/controller/ShiroHandlerController.java
+++ src/main/java/com/tsingsoft/controller/ShiroHandlerController.java
@@ -4,13 +4,13 @@ import com.tsingsoft.service.ShiroService;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.UsernamePasswordToken;
-import org.apache.shiro.authz.annotation.RequiresPermissions;
-import org.apache.shiro.authz.annotation.RequiresRoles;
 import org.apache.shiro.subject.Subject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/shiro")
@@ -20,7 +20,8 @@ public class ShiroHandlerController {
     private ShiroService shiroService;
 
     @RequestMapping("/testShiroAnnotation")
-    public  String testShiroAnnotation(){
+    public  String testShiroAnnotation(HttpSession session){
+        session.setAttribute("key","nihao 123");
         shiroService.testMethod();
         return "redirect:/list.jsp";
     }
--- src/main/java/com/tsingsoft/service/ShiroService.java
+++ src/main/java/com/tsingsoft/service/ShiroService.java
@@ -1,5 +1,6 @@
 package com.tsingsoft.service;
 
+import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authz.annotation.RequiresRoles;
 
 import java.util.Date;
@@ -9,5 +10,6 @@ public class ShiroService {
     @RequiresRoles({"admin"})
     public void testMethod(){
         System.out.println("test Method："+new Date());
+        System.out.println(SecurityUtils.getSubject().getSession().getAttribute("key"));
     }
 }
